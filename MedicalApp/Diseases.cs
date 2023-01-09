a8d6086ddb848de713317f3d43d04eafa3ef5519
AmruthaKottur <121652558+AmruthaKottur@users.noreply.github.com>
2023-01-09 16:24:06 +0530

    Add files via upload

---
+++ MedicalApp/Diseases.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleFrameworks.MedicalApp
+{
+    class Diseases
+    {
+        public string DiseaseName { get; set; }
+        public string severity { get; set; }
+        public string cause { get; set; }
+        public string description { get; set; }
+    }
+    class Symptoms : Diseases
+    {
+        public string DiseaseName { get; set; }
+        public string SymptomName { get; set; }
+        public string description { get; set; }
+    }
+    class PatientDetails : Symptoms
+    {
+        public string PatientName { get; set; }
+        public string SymptomName { get; set; }
+    }
+}
---
+++ MedicalApp/MedCollections.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleFrameworks.MedicalApp
+{
+    interface IDataComponent
+    {
+        void AddDisease(Diseases dis);
+        void AddSymptoms(Symptoms sym);
+
+       
+    }
+    class MedCollections : IDataComponent
+    {
+        private ArrayList _listOfDiseases = new ArrayList();
+        public void AddDisease(Diseases dis)
+        {
+            _listOfDiseases.Add(dis);
+            //throw new NotImplementedException();
+        }
+
+        public void AddSymptoms(Symptoms sym)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
